bbe453b2f607e2092592082c8eeaadb4e338214a
Lukáš Pavlíček <lukas.pavlicek@student.spsmb.cz>
2024-11-07 09:28:46 +0100

    router nicknames

--- client/src/pages/AppRoutes.jsx
+++ client/src/pages/AppRoutes.jsx
@@ -1,19 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./Home"
-import CreateForm from "./Monkey/CreateForm"
-import UpdateForm from "./Monkey/UpdateForm"
-import View from "./Monkey/View"
-import ViewAll from "./Monkey/ViewAll"
+import { CreateForm as CreateMonkey } from "./Monkey/CreateForm"
+import { UpdateForm as UpdateMonkey} from "./Monkey/UpdateForm"
+import { View as ViewMonkey } from "./Monkey/View"
+import { ViewAll as ViewAllMonkey } from "./Monkey/ViewAll"
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/create-monkey" element={<CreateForm/>}/>
-        <Route path="/update-monkey" element={<UpdateForm/>}/>
-        <Route path="/view-monkey" element={<View/>}/>
-        <Route path="/view-all-monkey" element={<ViewAll/>}/>
+        <Route path="/create-monkey" element={<CreateMonkey/>}/>
+        <Route path="/update-monkey" element={<UpdateMonkey/>}/>
+        <Route path="/view-monkey" element={<ViewMonkey/>}/>
+        <Route path="/view-all-monkey" element={<ViewAllMonkey/>}/>
       </Routes>
     </BrowserRouter>
   )
